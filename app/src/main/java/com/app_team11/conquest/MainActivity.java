6de3bafbc6e2e3d5176b4e50113c5d9dfcd03df2
jaydeeppateliconcern <jaydeep.patel@iconcern.net>
2017-10-06 00:31:47 -0400

    Project Initiated for build 1 ..

---
+++ app/src/main/java/com/app_team11/conquest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.app_team11.conquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Jaydeep9101 on 06-Oct-17.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
