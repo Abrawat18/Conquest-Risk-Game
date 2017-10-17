1dd079b5ea0510f74a5209d3916e75a03a616ca7
nigelr9 <nigelrodrigues07@gmail.com>
2017-10-17 04:41:21 +0000

    modified randomlyAssignCountries(...)

--- app/src/main/java/com/app_team11/conquest/utility/ReadMapUtility.java
+++ app/src/main/java/com/app_team11/conquest/utility/ReadMapUtility.java
@@ -291,11 +291,13 @@ public class ReadMapUtility {
         Collections.shuffle(Territories);
         while (Territories.size() > 0 && Tcount<Territories.size())
         {
+            //for (int i = 1; i <= Players.size(); i++) 
             System.out.println("Tcount value: "+Tcount);
             while (Players.size()> 0) 
             {
-                System.out.println(Territories.get(Tcount).getTerritoryName()+" Territory assigned to" +Players.get(Pcount).getPlayerNo());
-                Players.get(Pcount).addOwnedTerritorySingle(Territories.get(Tcount));
+                System.out.println(Territories.get(Tcount).getTerritoryName()+" Territory assigned to" +Players.get(Pcount).getPlayerId());
+                //Players.get(Pcount).addOwnedTerritorySingle(Territories.get(Tcount));
+                Territories.get(Tcount).setTerritoryOwner(Players.get(Pcount));
                 if (Pcount == Players.size()-1) 
                 {
                     Pcount = -1;
@@ -305,7 +307,7 @@ public class ReadMapUtility {
             }
             Tcount++;
             Pcount++;
-        printPlayerList();
+
         }
         return Players;
     }
