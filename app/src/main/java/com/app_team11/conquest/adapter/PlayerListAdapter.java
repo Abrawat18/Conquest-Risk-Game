b5526df7e66072765c938df065efb83cff29f0df
jaydeeppateliconcern <jaydeep.patel@iconcern.net>
2017-10-19 17:24:34 -0400

    Connecting gameplay with startupphasecontroller

---
+++ app/src/main/java/com/app_team11/conquest/adapter/PlayerListAdapter.java
@@ -0,0 +1,70 @@
+package com.app_team11.conquest.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.app_team11.conquest.R;
+import com.app_team11.conquest.model.Player;
+
+import java.util.List;
+
+/**
+ * Created by Jaydeep9101 on 19-Oct-17.
+ */
+
+public class PlayerListAdapter extends BaseAdapter {
+    private final LayoutInflater inflater;
+    private List<Player> playerList;
+
+    public PlayerListAdapter(Context context, List<Player> playerList) {
+        this.playerList = playerList;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return playerList.size();
+    }
+
+    @Override
+    public Player getItem(int position) {
+        return playerList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder = null;
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.list_row_player,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.textPlayerName = (TextView) convertView.findViewById(R.id.text_player_name);
+            viewHolder.textPlayerUnAssignedArmy = (TextView) convertView.findViewById(R.id.text_player_un_assigned_army);
+            convertView.setTag(viewHolder);
+        }
+        viewHolder = (ViewHolder) convertView.getTag();
+        viewHolder.textPlayerName.setText("Player: "+getItem(position).getPlayerId());
+        viewHolder.textPlayerUnAssignedArmy.setText(getItem(position).getAvailableArmyCount());
+        if(getItem(position).isMyTurn()){
+            viewHolder.textPlayerName.setTypeface(null, Typeface.BOLD);
+            viewHolder.textPlayerName.setTextColor(Color.GREEN);
+        }
+        return convertView;
+    }
+
+    class ViewHolder{
+        private TextView textPlayerName;
+        private TextView textPlayerUnAssignedArmy;
+    }
+}
--- app/src/main/java/com/app_team11/conquest/view/GamePlayActivity.java
+++ app/src/main/java/com/app_team11/conquest/view/GamePlayActivity.java
@@ -1,53 +1,77 @@
 package com.app_team11.conquest.view;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.app_team11.conquest.R;
+import com.app_team11.conquest.adapter.PlayerListAdapter;
 import com.app_team11.conquest.global.Constants;
+import com.app_team11.conquest.interfaces.SurfaceOnTouchListner;
 import com.app_team11.conquest.model.GameMap;
+import com.app_team11.conquest.model.Player;
 import com.app_team11.conquest.model.Territory;
-import com.app_team11.conquest.utility.ReadMapUtility;
+import com.app_team11.conquest.utility.MathUtility;
 
 /**
  * Created by RADHEY on 10/15/2017.
  */
 
 
-public class GamePlayActivity extends Activity implements View.OnTouchListener {
+public class GamePlayActivity extends Activity implements View.OnTouchListener, View.OnClickListener {
 
     private GameMap map;
     private SurfaceView surface;
     private Canvas canvas;
+    private SurfaceOnTouchListner surfaceOnTouchListner;
+    private Player playerTurn;
+    private ListView listPlayer;
+    private Button btnStartFortificationPhase;
+    private PlayerListAdapter playerListAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
         initializeView();
 
         initialization();
 
     }
 
+
     private void initializeView() {
+        listPlayer = (ListView) findViewById(R.id.list_player);
+        btnStartFortificationPhase = (Button) findViewById(R.id.btn_start_fortification_phase);
         surface = (SurfaceView) findViewById(R.id.surface);
         surface.setOnTouchListener(this);
         surface.getHolder().addCallback(surfaceCallback);
+
+        btnStartFortificationPhase.setOnClickListener(this);
     }
 
     private void initialization() {
         StartUpPhaseController.getInstance().setContext(this).startStartUpPhase();
+        if (getMap().getPlayerList() != null) {
+            playerListAdapter = new PlayerListAdapter(this, getMap().getPlayerList());
+            listPlayer.setAdapter(playerListAdapter);
+        }
+    }
+
+
+
+    public void setSurfaceOnTouchListner(SurfaceOnTouchListner surfaceOnTouchListner) {
+        this.surfaceOnTouchListner = surfaceOnTouchListner;
     }
 
     public void setMap(GameMap map) {
@@ -56,6 +80,22 @@ public class GamePlayActivity extends Activity implements View.OnTouchListener {
 
     public GameMap getMap() {
         return map;
+    }
+
+
+    public Territory getTerritoryAtSelectedPoint(int x, int y) {
+        for (Territory territory : map.getTerritoryList()) {
+            double distanceFromTerritory = MathUtility.getInstance().getDistance(x, y, territory.getCenterPoint().x, territory.getCenterPoint().y);
+            if (distanceFromTerritory < Constants.TERRITORY_RADIUS) {
+                return territory;
+            }
+        }
+        return null;
+    }
+
+
+    public void setPlayerTurn(Player player) {
+        playerTurn = player;
     }
 
     private void showMap() {
@@ -93,7 +133,15 @@ public class GamePlayActivity extends Activity implements View.OnTouchListener {
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        surfaceOnTouchListner.onTouch(v, event);
         return false;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start_fortification_phase:
+                break;
+        }
+    }
 }
