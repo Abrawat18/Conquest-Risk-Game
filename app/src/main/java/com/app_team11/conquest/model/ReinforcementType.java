976bb4e5b99ee901dffde2db7ed5b6e8551f65af
RADHEY <radhey.baps@gmail.com>
2017-11-04 18:24:55 -0400

    JavaDoc in the Model Package->Reinforcement Type Class

--- app/src/main/java/com/app_team11/conquest/model/ReinforcementType.java
+++ app/src/main/java/com/app_team11/conquest/model/ReinforcementType.java
@@ -3,6 +3,7 @@ package com.app_team11.conquest.model;
 import java.util.List;
 
 /**
+ * This is model code responsible for storing the reinforcement armies based on card and armies from other sources
  * Created by Vasu on 08-10-2017.
  * @version 1.0.0
  */
@@ -13,26 +14,50 @@ public class ReinforcementType {
     private int otherTotalReinforcement;
     private List<Territory> matchedTerritoryList;
 
+    /**
+     * Returns the matched territory list
+     * @return MatchedTerritoryList
+     */
     public List<Territory> getMatchedTerritoryList() {
         return matchedTerritoryList;
     }
 
+    /**
+     * Sets the matched territory list
+     * @param matchedTerritoryList
+     */
     public void setMatchedTerritoryList(List<Territory> matchedTerritoryList) {
         this.matchedTerritoryList = matchedTerritoryList;
     }
 
+    /**
+     * Returns the reinforcement matched territory card
+     * @return matchedTerrCardReinforcement
+     */
     public int getMatchedTerrCardReinforcement() {
         return matchedTerrCardReinforcement;
     }
 
+    /**
+     * Sets the reinforcement matched territory card
+     * @param matchedTerrCardReinforcement
+     */
     public void setMatchedTerrCardReinforcement(int matchedTerrCardReinforcement) {
         this.matchedTerrCardReinforcement = matchedTerrCardReinforcement;
     }
 
+    /**
+     * Returns the other total reinforcement
+     * @return otherTotalReinforcement
+     */
     public int getOtherTotalReinforcement() {
         return otherTotalReinforcement;
     }
 
+    /**
+     * Sets other total reinforcement
+     * @param otherTotalReinforcement
+     */
     public void setOtherTotalReinforcement(int otherTotalReinforcement) {
         this.otherTotalReinforcement = otherTotalReinforcement;
     }
