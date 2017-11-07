9cba7856fe39dd3c8dfaf0a110348217508a789b
nigelr9 <nigelrodrigues07@gmail.com>
2017-11-07 16:09:37 -0500

    Refactoring test classes

--- app/src/test/java/com/app_team11/conquest/StartupPhaseTestSuite.java
+++ app/src/test/java/com/app_team11/conquest/StartupPhaseTestSuite.java
@@ -3,12 +3,11 @@ package com.app_team11.conquest;
 /**
  * Created by Nigel on 20-Oct-17.
  */
-import com.app_team11.conquest.model.GameMapTest;
+import com.app_team11.conquest.model.GameMapTest1;
 import com.app_team11.conquest.model.InvalidTerritoryAdditionTest;
 import com.app_team11.conquest.model.RemoveTerritoryTest;
 import com.app_team11.conquest.model.TerritoryTest;
 import com.app_team11.conquest.utility.InvalidMapTest;
-import com.app_team11.conquest.utility.ReadMapUtility;
 import com.app_team11.conquest.utility.ReadMapUtilityTest;
 import com.app_team11.conquest.utility.ValidMapTest;
 import com.app_team11.conquest.model.CardTest;
@@ -20,7 +19,7 @@ import org.junit.runners.Suite;
     @RunWith(Suite.class)
 
     @Suite.SuiteClasses({
-            GameMapTest.class,
+            GameMapTest1.class,
             InvalidTerritoryAdditionTest.class,
             RemoveTerritoryTest.class,
             TerritoryTest.class,
