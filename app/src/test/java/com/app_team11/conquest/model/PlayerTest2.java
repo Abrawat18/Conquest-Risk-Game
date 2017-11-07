6ff63130bfe43af5d7bad6fba4fee67ed7506b2d
nigelr9 <nigelrodrigues07@gmail.com>
2017-11-07 08:03:34 -0500

    Player Test 2

---
+++ app/src/test/java/com/app_team11/conquest/model/PlayerTest2.java
@@ -0,0 +1,37 @@
+package com.app_team11.conquest.model;
+
+import com.app_team11.conquest.global.Constants;
+import com.app_team11.conquest.utility.ConfigurableMessage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Nigel on 06-Nov-17.
+ * Validation conditions to check for sufficient armies
+ */
+
+public class PlayerTest2 {
+    Territory attackerTerritory;
+    ConfigurableMessage cm;
+
+    @Before
+    public void setUp()
+    {
+        attackerTerritory=new Territory();
+        attackerTerritory.setArmyCount(3);
+    }
+    @Test
+    public void validateNumberOfArmies()
+    {
+        cm=new Player().hasSufficientArmies(attackerTerritory);
+        assertEquals(Constants.MSG_SUCC_CODE,cm.getMsgCode());
+
+        attackerTerritory.setArmyCount(1);
+        cm=new Player().hasSufficientArmies(attackerTerritory);
+        assertEquals(Constants.MSG_FAIL_CODE,cm.getMsgCode());
+    }
+}
