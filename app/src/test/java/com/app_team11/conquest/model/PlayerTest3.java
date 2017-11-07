c206e0d005c3b71965577950ab88ae73a3f3f461
nigelr9 <nigelrodrigues07@gmail.com>
2017-11-07 08:11:57 -0500

    PlayerTest3

---
+++ app/src/test/java/com/app_team11/conquest/model/PlayerTest3.java
@@ -0,0 +1,37 @@
+package com.app_team11.conquest.model;
+
+import com.app_team11.conquest.global.Constants;
+import com.app_team11.conquest.utility.ConfigurableMessage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Nigel on 07-Nov-17.
+ * Checks whether defender territory can be captured
+ */
+
+public class PlayerTest3 {
+    Territory defenderTerritory;
+    ConfigurableMessage cm;
+    @Before
+    public void setUp()
+    {
+        defenderTerritory=new Territory();
+        defenderTerritory.setArmyCount(3);
+    }
+    @Test
+    public void validateCanBeAttacked()
+    {
+        cm=new Player().canContinueAttackOnThisTerritory(defenderTerritory);
+        assertEquals(Constants.MSG_SUCC_CODE,cm.getMsgCode());
+
+        defenderTerritory.setArmyCount(defenderTerritory.getArmyCount()-3);
+        cm=new Player().canContinueAttackOnThisTerritory(defenderTerritory);
+        assertEquals(Constants.MSG_FAIL_CODE,cm.getMsgCode());
+
+
+    }
+}
