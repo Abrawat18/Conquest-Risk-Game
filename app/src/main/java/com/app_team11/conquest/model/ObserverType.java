536e483679b8ce4ac8ae7ecdd376f739baf772de
RADHEY <radhey.baps@gmail.com>
2017-11-08 03:08:19 -0500

    JavaDoc in the Model-GamePhase,ObserverType

--- app/src/main/java/com/app_team11/conquest/model/ObserverType.java
+++ app/src/main/java/com/app_team11/conquest/model/ObserverType.java
@@ -1,6 +1,7 @@
 package com.app_team11.conquest.model;
 
 /**
+ * World domination view and phase view constant definition
  * Created by Jaydeep9101 on 08-Nov-17.
  */
 
@@ -9,10 +10,18 @@ public class ObserverType {
     public static final int PHASE_VIEW_UPDATE_TYPE=2;
     private int observerType;
 
+    /**
+     * Returns the observer type
+     * @return type of observer
+     */
     public int getObserverType() {
         return observerType;
     }
 
+    /**
+     * Sets the observer type
+     * @param observerType
+     */
     public void setObserverType(int observerType) {
         this.observerType = observerType;
     }
