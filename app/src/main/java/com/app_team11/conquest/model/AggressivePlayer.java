8711bcbe2ed43b6db74b7c0fd0c34ff8df223cbf
nigelr9 <nigelrodrigues07@gmail.com>
2017-11-23 15:55:03 -0500

    Aggressive Player logic modification

--- app/src/main/java/com/app_team11/conquest/model/AggressivePlayer.java
+++ app/src/main/java/com/app_team11/conquest/model/AggressivePlayer.java
@@ -1,13 +1,17 @@
 package com.app_team11.conquest.model;
 
+import android.util.Log;
+
 import com.app_team11.conquest.global.Constants;
 import com.app_team11.conquest.utility.ConfigurableMessage;
 import com.app_team11.conquest.utility.FileManager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Observable;
+import java.util.Random;
 
 /**
  * Created by Nigel on 22-Nov-17.
@@ -264,12 +268,13 @@ public class AggressivePlayer extends Observable
     public Territory getStrongestTerritory(List<Territory> playerTerritoryList)
     {
         Territory strongestTerritory=null;
+        List<Territory> territoryList=new ArrayList<Territory>(playerTerritoryList);
         List<Integer> armyList=new ArrayList<Integer>();
-        for(Territory territory:playerTerritoryList)
+        for(Territory territory:territoryList)
         {
             armyList.add(territory.getArmyCount());
         }
-        for(Territory territory:playerTerritoryList)
+        for(Territory territory:territoryList)
         {
             if(territory.getArmyCount()== Collections.max(armyList))
             {
@@ -277,7 +282,23 @@ public class AggressivePlayer extends Observable
                 break;
             }
         }
+        for(Territory neighbourTerr:strongestTerritory.getNeighbourList())
+        {
+
+
+        }
         return strongestTerritory;
+    }
+
+
+    public Boolean ownedNeighbourTerritories(Territory territory)
+    {
+        for(Territory terr : territory.getNeighbourList())
+        {
+            if(terr.getTerritoryOwner().getPlayerId()!=territory.getTerritoryOwner().getPlayerId())
+                return true;
+        }
+        return false;
     }
 
     /**
@@ -312,6 +333,244 @@ public class AggressivePlayer extends Observable
             return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.FORTIFICATION_FAILURE);
     }
 
+    /**
+     * Checks whether player is attacking an already owned territory
+     * @param defenderTerritory
+     * @return whether adjacent or not
+     */
+
+    public ConfigurableMessage isAdjacentTerritory(Territory defenderTerritory) {
+        if ((this.strongestTerritory.getNeighbourList().contains(defenderTerritory)) && (!this.strongestTerritory.getTerritoryOwner().equals(defenderTerritory.getTerritoryOwner()))) {
+            return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.SUCCESS);
+        }
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.NOT_ADJACENT_TERRITORY);
+    }
+
+    /**
+     * Method to check whether strongest territory has sufficient armies
+     * @return army status
+     */
+     public ConfigurableMessage hasSufficientArmies() {
+        if (this.strongestTerritory.getArmyCount() >= 2)
+            return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.SUCCESS);
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.INSUFFUCIENT_ARMIES);
+    }
+
+    /**
+     * Checks whether attack can be continued
+     *
+     * @param defenderTerritory
+     * @return
+     */
+    public ConfigurableMessage canContinueAttackOnThisTerritory(Territory defenderTerritory) {
+        if (defenderTerritory.getArmyCount() == 0)
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.NO_ARMIES);
+        return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.SUCCESS);
+    }
+
+    /**
+     * Validate the attack
+     *
+     * @param defenderTerritory
+     * @return
+     */
+    public ConfigurableMessage validateAttackBetweenTerritories(Territory defenderTerritory) {
+        ConfigurableMessage isAdjacenTerritories = isAdjacentTerritory(defenderTerritory);
+        ConfigurableMessage hasSufficientArmiesForAttack = hasSufficientArmies();
+        ConfigurableMessage canContinueAttack = canContinueAttackOnThisTerritory(defenderTerritory);
+
+        if (isAdjacenTerritories.getMsgCode() == 0)
+            return isAdjacenTerritories;
+        else if (hasSufficientArmiesForAttack.getMsgCode() == 0) {
+            return hasSufficientArmiesForAttack;
+        } else if (canContinueAttack.getMsgCode() == 0) {
+            return canContinueAttack;
+        } else {
+            return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.SUCCESS);
+        }
+    }
+
+
+    /**
+     * The attack phase method
+     *
+     * @param attackerTerritory
+     * @param defenderTerritory
+     * @param attackerDice
+     * @param defenderDice
+     */
+    public ConfigurableMessage attackPhase(Territory defenderTerritory, int attackerDice, int defenderDice) {
+        setNumberOfDiceRolled(attackerDice);
+        if (this.strongestTerritory.getArmyCount() <= attackerDice) {
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.ATTACKER_DICE);
+        } else if (defenderTerritory.getArmyCount() < defenderDice) {
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.CHOOSE_LESS_NUMBER_DICE);
+        }
+        String messageAttack = "Attacking City " + this.strongestTerritory.getTerritoryName();
+        PhaseViewModel.getInstance().addPhaseViewContent(messageAttack);
+        String messageDefender = "Attacked City " + defenderTerritory.getTerritoryName();
+        PhaseViewModel.getInstance().addPhaseViewContent(messageDefender);
+
+        //Validations are true for attacking and hence proceeding with logic
+        //Load dice values
+        List<Integer> attackerDiceValues = getRandomDiceValues(attackerDice);
+        List<Integer> defenderDiceValues = getRandomDiceValues(defenderDice);
+
+        try {
+            Log.e(TAG, "Attacker Dice:" + attackerDiceValues.toString());
+            Log.e(TAG, "Defender Dice:" + defenderDiceValues.toString());
+        } catch (Exception e) {
+
+        }
+        int attackerDiceValue = 0, defenderDiceValue = 0;
+        int attackerWonCounter = 0;
+
+        Collections.sort(attackerDiceValues, Collections.reverseOrder());
+        Collections.sort(defenderDiceValues, Collections.reverseOrder());
+
+        String messageAttackDice = "Attacker Dice " + Arrays.toString(attackerDiceValues.toArray());
+        PhaseViewModel.getInstance().addPhaseViewContent(messageAttackDice);
+        String messageDefenderDice = "Defender Dice " + Arrays.toString(defenderDiceValues.toArray());
+        PhaseViewModel.getInstance().addPhaseViewContent(messageDefenderDice);
+        try {
+            FileManager.getInstance().writeLog(messageAttack);
+            FileManager.getInstance().writeLog(messageDefender);
+            FileManager.getInstance().writeLog(messageAttackDice);
+            FileManager.getInstance().writeLog(messageDefenderDice);
+        } catch (Exception e) {
+            // e.printStackTrace();
+        }
+        while (defenderDiceValues.size() > 0) {
+            attackerDiceValue = attackerDiceValues.get(0);
+            defenderDiceValue = defenderDiceValues.get(0);
+
+            //if attacker wins
+            if (attackerDiceValue > defenderDiceValue) {
+                defenderTerritory.setArmyCount(defenderTerritory.getArmyCount() - 1);
+                attackerWonCounter++;
+            } else {
+                this.strongestTerritory.setArmyCount(this.strongestTerritory.getArmyCount() - 1);
+                attackerWonCounter--;
+            }
+            attackerDiceValues.remove(0);
+            defenderDiceValues.remove(0);
+        }
+        if (attackerWonCounter > 0) {
+            String message = "Player" + this.strongestTerritory.getTerritoryOwner().getPlayerId() + " won";
+            PhaseViewModel.getInstance().addPhaseViewContent(message);
+            try {
+                FileManager.getInstance().writeLog(message);
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.ATTACKER_WON);
+        } else {
+            String message = "Player" + defenderTerritory.getTerritoryOwner().getPlayerId() + " won";
+            PhaseViewModel.getInstance().addPhaseViewContent(message);
+            try {
+                FileManager.getInstance().writeLog(message);
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.ATTACKER_LOST);
+
+        }
+    }
+
+    private int numberOfDiceRolled;
+
+    /**
+     * Returns number of dice rolled
+     * @return
+     */
+    public int getNumberOfDiceRolled() {
+        return numberOfDiceRolled;
+    }
+
+    /**
+     * Set the number of dice rolled
+     * @param numberOfDiceRolled
+     */
+    public void setNumberOfDiceRolled(int numberOfDiceRolled) {
+        this.numberOfDiceRolled = numberOfDiceRolled;
+    }
+
+    /**
+     * This method is for conditions related to capturing a territory
+     *
+     * @param defenderTerritory
+     * @param moveArmiesToCapturedTerritory
+     * @return
+     */
+    public ConfigurableMessage captureTerritory(Territory defenderTerritory, int moveArmiesToCapturedTerritory) {
+        if (this.strongestTerritory.getArmyCount() - moveArmiesToCapturedTerritory == 0) {
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.LEAVE_ONE_ARMY);
+        } else if (defenderTerritory.getArmyCount() == 0 && moveArmiesToCapturedTerritory < this.numberOfDiceRolled)
+            return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.PLACE_MORE_ARMIES);
+        else {
+            this.strongestTerritory.setArmyCount(this.strongestTerritory.getArmyCount() - moveArmiesToCapturedTerritory);
+            defenderTerritory.setArmyCount(moveArmiesToCapturedTerritory);
+            defenderTerritory.setTerritoryOwner(this.strongestTerritory.getTerritoryOwner());
+        }
+        String message = "Player " + this.strongestTerritory.getTerritoryOwner().getPlayerId() + " has captured " + defenderTerritory.getTerritoryName();
+        PhaseViewModel.getInstance().addPhaseViewContent(message);
+        try {
+            FileManager.getInstance().writeLog(message);
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        ObserverType observerType = new ObserverType();
+        observerType.setObserverType(ObserverType.WORLD_DOMINATION_TYPE);
+        setChanged();
+        notifyObservers(observerType);
+        return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.SUCCESS);
+    }
+
+
+    /**
+     * This method returns the highest value from a given list.
+     *
+     * @param diceArray
+     * @return Maximum element from the list
+     */
+    public int getHighestValue(List<Integer> diceArray) {
+        Collections.sort(diceArray, Collections.reverseOrder());
+        return diceArray.get(0);
+    }
+
+    /**
+     * This method deletes the element from the list.
+     *
+     * @param diceArray
+     * @param element
+     * @return the modified list
+     */
+    public int[] deleteElement(int diceArray[], int element) {
+        for (int i = 0; i < diceArray.length; i++) {
+            if (diceArray[i] == element) {
+                for (int j = i; j < (diceArray.length - 1); j++) {
+                    diceArray[j] = diceArray[j + 1];
+                }
+                break;
+            }
+        }
+        return diceArray;
+    }
+
+
+    /**
+     * Generates random dice values depending on number of attacker/defender dice
+     *
+     * @param arraySize
+     * @return randomly generated dice array
+     */
+    public static List<Integer> getRandomDiceValues(int arraySize) {
+        List<Integer> randomArray = new ArrayList<>();
+        for (int i = 0; i < arraySize; i++) {
+            randomArray.add(1 + new Random().nextInt(6));
+        }
+        return randomArray;
+    }
 
 
 
