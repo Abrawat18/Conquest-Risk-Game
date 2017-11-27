de373c8998a0205c16dfef15d31d2bf58e8cb30a
Jaydeep <jaydeeppatel292@gmail.com>
2017-11-27 02:08:07 -0500

    creating player strategy

---
+++ app/src/main/java/com/app_team11/conquest/model/RandomPlayerStrategy.java
@@ -0,0 +1,29 @@
+package com.app_team11.conquest.model;
+
+import com.app_team11.conquest.interfaces.gamePhaseListner;
+
+/**
+ * Created by Jaydeep on 11/26/2017.
+ */
+
+public class RandomPlayerStrategy implements gamePhaseListner {
+    @Override
+    public void startupPhase() {
+
+    }
+
+    @Override
+    public void reInforcementPhase() {
+
+    }
+
+    @Override
+    public void attackPhase() {
+
+    }
+
+    @Override
+    public void fortificationPhase() {
+
+    }
+}
