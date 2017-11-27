349842a4c26e65b3de049b26a08a16cd18eccc46
Jaydeep <jaydeeppatel292@gmail.com>
2017-11-27 03:01:01 -0500

    Adding aggressive cheater and human player strategy configuration

--- app/src/main/java/com/app_team11/conquest/model/CheaterPlayerStrategy.java
+++ app/src/main/java/com/app_team11/conquest/model/CheaterPlayerStrategy.java
@@ -15,16 +15,33 @@ public class CheaterPlayerStrategy implements GamePhaseListner {
 
     @Override
     public void reInforcementPhase(GameMap gameMap, Player player) {
-
+        for (Territory territory : gameMap.getTerrForPlayer(player)) {
+            territory.setArmyCount(2 * territory.getArmyCount());
+        }
     }
 
     @Override
     public void attackPhase(GameMap gameMap, Player player) {
-
+        for (Territory territory : gameMap.getTerrForPlayer(player)) {
+            for (Territory neighbourTerr : territory.getNeighbourList()) {
+                neighbourTerr.setTerritoryOwner(player);
+            }
+        }
     }
 
     @Override
     public void fortificationPhase(GameMap gameMap, Player player) {
-
+        for (Territory territory : gameMap.getTerrForPlayer(player)) {
+            boolean isNeighbourOtherOwner = false;
+            for (Territory neighbourTerr : territory.getNeighbourList()) {
+                if (neighbourTerr.getTerritoryOwner().getPlayerId() != player.getPlayerId()) {
+                    isNeighbourOtherOwner = true;
+                    break;
+                }
+            }
+            if (isNeighbourOtherOwner) {
+                territory.setArmyCount(2 * territory.getArmyCount());
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/app_team11/conquest/model/HumanPlayerStrategy.java
@@ -0,0 +1,29 @@
+package com.app_team11.conquest.model;
+
+import com.app_team11.conquest.interfaces.GamePhaseListner;
+
+/**
+ * Created by Jaydeep on 11/27/2017.
+ */
+
+public class HumanPlayerStrategy implements GamePhaseListner {
+    @Override
+    public void startupPhase(GameMap gameMap, Player player) {
+
+    }
+
+    @Override
+    public void reInforcementPhase(GameMap gameMap, Player player) {
+
+    }
+
+    @Override
+    public void attackPhase(GameMap gameMap, Player player) {
+
+    }
+
+    @Override
+    public void fortificationPhase(GameMap gameMap, Player player) {
+
+    }
+}
