8fae68027a2ba7773f1ca514493b24763c3b0367
Vasu <jain.vasu92@gmail.com>
2017-11-27 04:01:18 -0500

    changes for aggressive player strategy

--- app/src/main/java/com/app_team11/conquest/interfaces/PlayerStrategyListener.java
+++ app/src/main/java/com/app_team11/conquest/interfaces/PlayerStrategyListener.java
@@ -3,17 +3,18 @@ package com.app_team11.conquest.interfaces;
 import com.app_team11.conquest.model.GameMap;
 import com.app_team11.conquest.model.Player;
 import com.app_team11.conquest.model.ReinforcementType;
+import com.app_team11.conquest.utility.ConfigurableMessage;
 
 /**
  * Created by Jaydeep on 11/26/2017.
  */
 
 public interface PlayerStrategyListener {
-    void startupPhase(GameMap gameMap, Player player);
+    ConfigurableMessage startupPhase(GameMap gameMap, Player player);
 
-    void reInforcementPhase(ReinforcementType reinforcementType, GameMap gameMap, Player player);
+    ConfigurableMessage reInforcementPhase(ReinforcementType reinforcementType, GameMap gameMap, Player player);
 
-    void attackPhase(GameMap gameMap, Player player);
+    ConfigurableMessage attackPhase(GameMap gameMap, Player player);
 
-    void fortificationPhase(GameMap gameMap, Player player);
+    ConfigurableMessage fortificationPhase(GameMap gameMap, Player player);
 }
--- app/src/main/java/com/app_team11/conquest/model/AggressivePlayerStrategy.java
+++ app/src/main/java/com/app_team11/conquest/model/AggressivePlayerStrategy.java
@@ -1,6 +1,8 @@
 package com.app_team11.conquest.model;
 
+import com.app_team11.conquest.global.Constants;
 import com.app_team11.conquest.interfaces.PlayerStrategyListener;
+import com.app_team11.conquest.utility.ConfigurableMessage;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,30 +13,47 @@ import java.util.Comparator;
 
 public class AggressivePlayerStrategy implements PlayerStrategyListener {
     @Override
-    public void startupPhase(GameMap gameMap, Player player) {
-
+    public ConfigurableMessage startupPhase(GameMap gameMap, Player player) {
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.FORTIFICATION_FAILURE_STRATEGY);
     }
 
     @Override
-    public void reInforcementPhase(ReinforcementType reinforcementType, GameMap gameMap, Player player) {
-
+    public ConfigurableMessage reInforcementPhase(ReinforcementType reinforcementType, GameMap gameMap, Player player) {
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.FORTIFICATION_FAILURE_STRATEGY);
     }
 
 
     @Override
-    public void attackPhase(GameMap gameMap, Player player) {
-
+    public ConfigurableMessage attackPhase(GameMap gameMap, Player player) {
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.FORTIFICATION_FAILURE_STRATEGY);
     }
 
     @Override
-    public void fortificationPhase(GameMap gameMap, Player player) {
+    public ConfigurableMessage fortificationPhase(GameMap gameMap, Player player) {
+        boolean fortificationFlag = false;
         Collections.sort(gameMap.getTerrForPlayer(player), new Comparator<Territory>() {
             @Override public int compare(Territory t1, Territory t2) {
                 return t2.getArmyCount()- t1.getArmyCount();
             }
         });
         for(Territory territory : gameMap.getTerrForPlayer(player)){
-
-        }
+            Collections.sort(territory.getNeighbourList(), new Comparator<Territory>() {
+                @Override public int compare(Territory t1, Territory t2) {
+                    return t2.getArmyCount()- t1.getArmyCount();
+                }
+            });
+            for(Territory neighbourTerr : territory.getNeighbourList()) {
+                if (neighbourTerr.getTerritoryOwner() == player && neighbourTerr.getArmyCount()>1){
+                    territory.setArmyCount(territory.getArmyCount()+(neighbourTerr.getArmyCount()-1));
+                    neighbourTerr.setArmyCount(1);
+                    fortificationFlag = true;
+                    break;
+                }
+            }
+            if(fortificationFlag){
+                return new ConfigurableMessage(Constants.MSG_SUCC_CODE, Constants.FORTIFICATION_SUCCESS);
+            }
+            }
+        return new ConfigurableMessage(Constants.MSG_FAIL_CODE, Constants.FORTIFICATION_FAILURE_STRATEGY);
     }
 }
