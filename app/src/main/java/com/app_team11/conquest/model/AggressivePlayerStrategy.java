02f4ba476e477a65e6ea450e818b7ddfefeda5d0
Vasu <jain.vasu92@gmail.com>
2017-11-27 03:35:11 -0500

    changes for aggressive player strategy

--- app/src/main/java/com/app_team11/conquest/model/AggressivePlayerStrategy.java
+++ app/src/main/java/com/app_team11/conquest/model/AggressivePlayerStrategy.java
@@ -2,6 +2,9 @@ package com.app_team11.conquest.model;
 
 import com.app_team11.conquest.interfaces.PlayerStrategyListener;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Created by Jaydeep on 11/27/2017.
  */
@@ -25,6 +28,13 @@ public class AggressivePlayerStrategy implements PlayerStrategyListener {
 
     @Override
     public void fortificationPhase(GameMap gameMap, Player player) {
+        Collections.sort(gameMap.getTerrForPlayer(player), new Comparator<Territory>() {
+            @Override public int compare(Territory t1, Territory t2) {
+                return t2.getArmyCount()- t1.getArmyCount();
+            }
+        });
+        for(Territory territory : gameMap.getTerrForPlayer(player)){
 
+        }
     }
 }
