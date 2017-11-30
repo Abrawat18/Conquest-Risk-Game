46aab011fa6f1355bb8eab289ee88406b4954fd4
nigelr9 <nigelrodrigues07@gmail.com>
2017-11-30 14:36:14 -0500

    TournamentModeTestSuite

---
+++ app/src/test/java/com/app_team11/conquest/TournamentModeTestSuite.java
@@ -0,0 +1,37 @@
+package com.app_team11.conquest;
+
+import com.app_team11.conquest.model.AddTerritoryTest;
+import com.app_team11.conquest.model.AggressivePlayerStrategyTest;
+import com.app_team11.conquest.model.BenevolentPlayerStrategyTest;
+import com.app_team11.conquest.model.CheaterPlayerStrategyTest;
+import com.app_team11.conquest.model.ConnectedGraphTest;
+import com.app_team11.conquest.model.GraphTest;
+import com.app_team11.conquest.model.InvalidMapTest;
+import com.app_team11.conquest.model.RandomPlayerStrategyTest;
+import com.app_team11.conquest.model.ReadFileTest;
+import com.app_team11.conquest.model.RemoveTerritoryTest;
+import com.app_team11.conquest.model.UnconnectedContinentTest;
+import com.app_team11.conquest.model.UnconnectedGraphTest;
+import com.app_team11.conquest.utility.ReadMapUtilityTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Nigel on 30-Nov-17.
+ * Tests to check the player strategies
+ */
+
+
+    @RunWith(Suite.class)
+
+    @Suite.SuiteClasses({
+            AggressivePlayerStrategyTest.class, //test for aggressive player strategy methods
+            BenevolentPlayerStrategyTest.class, //test for benevolent player strategy methods
+            RandomPlayerStrategyTest.class,     //test for random player strategy methods
+            CheaterPlayerStrategyTest.class     //test for cheater player strategy methods
+    })
+    public class TournamentModeTestSuite
+{
+
+}
